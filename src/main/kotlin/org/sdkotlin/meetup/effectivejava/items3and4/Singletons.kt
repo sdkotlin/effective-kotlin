9ca915212f677c46d27b557cd11761ac4f0909e8
Ian Brandt <ian@ianbrandt.com>
2018-06-06 11:47:58 -0700

    Added Singleton example.

--- src/main/kotlin/org/sdkotlin/meetup/effectivejava/items3and4/Singletons.kt
+++ src/main/kotlin/org/sdkotlin/meetup/effectivejava/items3and4/Singletons.kt
@@ -5,3 +5,19 @@ Item 3: Enforce the singleton property with a private constructor or an enum typ
 Item 4: Enforce noninstantiability with a private constructor
  */
 
+// Kotlin has the singleton pattern built into the language using `object`
+// definitions
+
+object HelloSingleton {
+
+	private const val mood = "great"
+
+	fun howYouDoin() {
+		println("${mood.capitalize()}, thanks for asking!")
+	}
+}
+
+fun main(args: Array<String>) {
+
+	HelloSingleton.howYouDoin()
+}
